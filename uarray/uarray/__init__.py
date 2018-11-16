ade65cdccaa15b2661c894b63c8ffad65e6a28e2
Saul Shanabrook <s.shanabrook@gmail.com>
2018-11-15 22:04:06 -0500

    Package with flit

--- uarray/uarray/__init__.py
+++ uarray/uarray/__init__.py
@@ -1,5 +1,10 @@
+"""Universal array library"""
 from . import ast_higher  # NOQA
 
 from .optimize import *  # type: ignore
 from .moa import *  # type: ignore
 from .logging import enable_logging
+
+
+__version__ = "0.1"
+
