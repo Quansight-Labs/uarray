cde7368a7bb907300f5ed256628f48a10630312b
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-11-27 09:11:28 -0500

    Adding mypy tests and filter source files

--- .nix/build.nix
+++ .nix/build.nix
@@ -2,25 +2,37 @@
 , pythonPackages ? pkgs.python36Packages
 }:
 
-pythonPackages.buildPythonPackage rec {
-  pname = "uarray";
-  version = "0.4";
-  format = "flit";
+rec {
+  uarray = pythonPackages.buildPythonPackage rec {
+    pname = "uarray";
+    version = "0.4";
+    format = "flit";
 
-  src = ../.;
+    src = with builtins; filterSource
+        (path: _:
+           !elem (baseNameOf path) [".git" "result" ".nix"])
+        ../.;
 
-  checkInputs = with pythonPackages; [ pytest nbval pytestcov numba ];
-  propagatedBuildInputs = with pythonPackages; [ matchpy numpy astunparse typing-extensions black ];
+    checkInputs = with pythonPackages; [
+      pytest nbval pytestcov numba mypy
+    ];
+    propagatedBuildInputs = with pythonPackages; [
+      matchpy numpy astunparse typing-extensions black
+    ];
 
-  checkPhase = ''
-    ${pythonPackages.python.interpreter} extract_readme_tests.py
-    pytest
-  '';
+    checkPhase = ''
+      # static type checking
+      mypy uarray
+      # build python source from readme
+      ${pythonPackages.python.interpreter} extract_readme_tests.py
+      pytest
+    '';
 
-  meta = with pkgs.lib; {
-    description = "Universal array library";
-    homepage = https://github.com/Quansight-Labs/uarray;
-    license = licenses.bsd0;
-    maintainers = [ maintainers.costrouc ];
+    meta = with pkgs.lib; {
+      description = "Universal array library";
+      homepage = https://github.com/Quansight-Labs/uarray;
+      license = licenses.bsd0;
+      maintainers = [ maintainers.costrouc ];
+    };
   };
 }
