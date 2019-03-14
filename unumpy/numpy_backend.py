6da93a66c9a7c11d10c8719739da35e142fcf016
Hameer Abbasi <hameerabbasi@yahoo.com>
2019-03-14 22:37:21 +0100

    Add np.generic.

--- unumpy/numpy_backend.py
+++ unumpy/numpy_backend.py
@@ -1,5 +1,5 @@
 import numpy as np
 from uarray.backend import TypeCheckBackend, register_backend
 
-NumpyBackend = TypeCheckBackend((np.ndarray, tuple, list), convertor=np.array)
+NumpyBackend = TypeCheckBackend((np.ndarray, np.generic, tuple, list), convertor=np.array)
 register_backend(NumpyBackend)
