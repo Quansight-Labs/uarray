93ff13ba3d51f6f2d947b5664b53dffde7440906
Hameer Abbasi <hameerabbasi@yahoo.com>
2019-03-27 13:16:48 +0100

    Add all NumPy ufuncs

--- unumpy/multimethods.py
+++ unumpy/multimethods.py
@@ -44,7 +44,7 @@ class UFunc(Dispatchable):
         out_args = arrays[self.nin:]
 
         out_args = in_args
-        out_kwargs = {'out': out_args}
+        out_kwargs = {**kwargs, 'out': out_args}
 
         return out_args, out_kwargs
 
@@ -62,11 +62,48 @@ class UFunc(Dispatchable):
     def _reduce_argreplacer(args, kwargs, arrays):
         out_args = list(args)
         out_args[1] = arrays[0]
-        return tuple(out_args), kwargs
+
+        out_kwargs = {**kwargs, 'out': arrays[1]}
+
+        return tuple(out_args), out_kwargs
 
     @argument_extractor(_reduce_argreplacer)
     def reduce(self, a, axis=0, dtype=None, out=None, keepdims=False):
         return (a, out)
 
+    @argument_extractor(_reduce_argreplacer)
+    def accumulate(self, a, axis=0, dtype=None, out=None):
+        return (a, out)
 
-add = UFunc()
+
+ufunc_list = [
+    # Math operations
+    'add', 'subtract', 'multiply', 'divide', 'logaddexp', 'logaddexp2',
+    'true_divide', 'floor_divide', 'negative', 'positive', 'power',
+    'remainder', 'mod', 'fmod', 'divmod', 'absolute', 'fabs', 'rint',
+    'sign', 'heaviside', 'conj', 'exp', 'exp2', 'log', 'log2', 'log10',
+    'expm1', 'log1p', 'sqrt', 'square', 'cbrt', 'reciprocal', 'gcd',
+    'lcm',
+
+    # Trigonometric functions
+    'sin', 'cos', 'tan', 'arcsin', 'arccos', 'arctan', 'arctan2',
+    'hypot', 'sinh', 'cosh', 'tanh', 'arcsinh', 'arccosh',
+    'arctanh', 'deg2rad', 'rad2deg',
+
+    # Bit-twiddling functions
+    'bitwise_and', 'bitwise_or', 'bitwise_xor', 'invert', 'left_shift',
+    'right_shift',
+
+    # Comparison functions
+    'greater', 'greater_equal', 'less', 'less_equal', 'not_equal',
+    'equal', 'logical_and', 'logical_or', 'logical_xor', 'logical_not',
+    'maximum', 'minimum', 'fmax', 'fmin',
+
+    # Floating functions
+    'isfinite', 'isinf', 'isnan', 'isnat', 'fabs', 'signbit', 'copysign',
+    'nextafter', 'spacing', 'modf', 'ldexp', 'frexp', 'fmod', 'floor',
+    'ceil', 'trunc'
+]
+
+for ufunc_name in ufunc_list:
+    globals()[ufunc_name] = UFunc()
--- unumpy/numpy_backend.py
+++ unumpy/numpy_backend.py
@@ -1,12 +1,18 @@
 import numpy as np
 from uarray.backend import TypeCheckBackend, register_backend, instance_multimethod
-from .multimethods import UFunc, add
+from .multimethods import UFunc, ufunc_list
+import unumpy.multimethods as multimethods
 
 NumpyBackend = TypeCheckBackend((np.ndarray, np.generic, tuple, list), convertor=np.array)
 register_backend(NumpyBackend)
 
-NumpyBackend.register_instance(add, np.add)
 instance_multimethod(NumpyBackend, UFunc.__call__)(np.ufunc.__call__)
+instance_multimethod(NumpyBackend, UFunc.reduce)(np.ufunc.reduce)
+instance_multimethod(NumpyBackend, UFunc.accumulate)(np.ufunc.accumulate)
 instance_multimethod(NumpyBackend, UFunc.nin.fget)(lambda x: x.nin)
 instance_multimethod(NumpyBackend, UFunc.nout.fget)(lambda x: x.nout)
 instance_multimethod(NumpyBackend, UFunc.types.fget)(lambda x: x.types)
+
+for ufunc_name in ufunc_list:
+    NumpyBackend.register_instance(getattr(multimethods, ufunc_name),
+                                   getattr(np, ufunc_name))
