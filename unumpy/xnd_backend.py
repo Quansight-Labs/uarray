e3631388682429ad3a49b713a177d1508eb3751a
Hameer Abbasi <hameerabbasi@yahoo.com>
2019-04-03 13:44:25 +0200

    Check-in the user-facing code.

--- unumpy/xnd_backend.py
+++ unumpy/xnd_backend.py
@@ -1,4 +1,4 @@
-from xnd import xnd
+import xnd
 import gumath.functions as fn
 import gumath as gu
 import unumpy.multimethods as multimethods
@@ -6,7 +6,7 @@ from .multimethods import UFunc, ufunc_list
 
 from uarray.backend import TypeCheckBackend, register_backend, multimethod
 
-XndBackend = TypeCheckBackend((xnd,), convertor=xnd)
+XndBackend = TypeCheckBackend((xnd.xnd,), convertor=xnd.array)
 register_backend(XndBackend)
 
 
@@ -23,3 +23,6 @@ for ufunc_name in ufunc_list:
     if hasattr(fn, ufunc_name):
         XndBackend.register_instance(getattr(multimethods, ufunc_name),
                                      getattr(fn, ufunc_name))
+
+multimethod(XndBackend, multimethods.array)(xnd.array)
+multimethod(XndBackend, multimethods.asarray)(xnd.array.from_buffer)
