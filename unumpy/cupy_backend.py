e51b40da939eb975257612ed160f549d1c70afcc
Hameer Abbasi <hameerabbasi@yahoo.com>
2019-05-09 00:24:47 -0500

    Add CuPy (testing locally for now)

--- unumpy/cupy_backend.py
+++ unumpy/cupy_backend.py
@@ -1,23 +1,22 @@
-import dask.array as da
+from uarray.backend import Backend, register_backend, register_implementation
+import cupy as cp
 import numpy as np
 import unumpy.multimethods as multimethods
 from .multimethods import ufunc, ufunc_list, ndarray, DispatchableInstance
 from typing import Dict
 import functools
 
-from uarray.backend import Backend, register_backend, register_implementation
-
-DaskBackend = Backend()
-register_backend(DaskBackend)
+CupyBackend = Backend()
+register_backend(CupyBackend)
 
 
 def compat_check(args):
     return not len(args) or \
-        any(isinstance(arg.value, da.core.Array) for arg in args
+        any(isinstance(arg.value, cp.ndarray) for arg in args
             if isinstance(arg, DispatchableInstance) and arg.value is not None)
 
 
-register_dask = functools.partial(register_implementation, backend=DaskBackend, compat_check=compat_check)
+register_dask = functools.partial(register_implementation, backend=CupyBackend, compat_check=compat_check)
 
 # experimental support for ufunc from Dask
 _ufunc_mapping: Dict[ufunc, np.ufunc] = {}
@@ -39,27 +38,27 @@ def replace_self(func):
 
 register_dask(ufunc.__call__)(replace_self(np.ufunc.__call__))
 register_dask(ufunc.reduce)(replace_self(np.ufunc.reduce))
-register_dask(multimethods.arange)(lambda start, stop, step, **kwargs: da.arange(start, stop, step, **kwargs))
+register_dask(multimethods.arange)(lambda start, stop, step, **kwargs: cp.arange(start, stop, step, **kwargs))
 
 
 for ufunc_name in ufunc_list:
     if hasattr(np, ufunc_name):
         _ufunc_mapping[getattr(multimethods, ufunc_name)] = getattr(np, ufunc_name)
 
-register_dask(multimethods.array)(da.core.Array)
-register_dask(multimethods.asarray)(da.asarray)
+register_dask(multimethods.array)(cp.array)
+register_dask(multimethods.asarray)(cp.asarray)
 
 
 def _generic(method, args, kwargs, dispatchable_args):
     if not compat_check(dispatchable_args):
         return NotImplemented
 
-    if not hasattr(da, method.__name__):
+    if not hasattr(cp, method.__name__):
         return NotImplemented
 
-    return getattr(da, method.__name__)(*args, **kwargs)
+    return getattr(cp, method.__name__)(*args, **kwargs)
 
 
-DaskBackend.register_implementation(None, _generic)
+CupyBackend.register_implementation(None, _generic)
 
-ndarray.register_convertor(DaskBackend, da.asarray)
+ndarray.register_convertor(CupyBackend, cp.asarray)
