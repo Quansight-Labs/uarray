c62a02afa07cb99ee738f47d15c037d92411ec4c
Hameer Abbasi <hameerabbasi@yahoo.com>
2022-02-16 08:39:51 +0100

    Minor maintenance fixes.

--- uarray/conftest.py
+++ uarray/conftest.py
@@ -5,16 +5,6 @@ import pytest  # type: ignore
 from .tests import example_helpers
 
 
-def pytest_cmdline_preparse(args):
-    try:
-        import pytest_black  # type: ignore
-    except ImportError:
-        pass
-    else:
-        args.append("--black")
-        print("uarray: Enabling pytest-black")
-
-
 @pytest.fixture(autouse=True)
 def add_namespaces(doctest_namespace):
     doctest_namespace["ua"] = uarray
