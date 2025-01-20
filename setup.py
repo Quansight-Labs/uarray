951903d8e0facb8aea13b4b63931a67795abc9ce
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2025-01-20 15:51:07 +0100

    First attempt at `abi3`.

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup
+from setuptools import setup, Extension
+from wheel.bdist_wheel import bdist_wheel
 
-setup(cmake_source_dir="./src")
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.10
+            return "cp310", "abi3", plat
+
+        return python, abi, plat
+
+
+setup(
+    cmake_source_dir="./src",
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
+)
