bef7bf5fd6a2c38b0f6c59bdba1768c937185371
Lucas Colley <lucas.colley8@gmail.com>
2026-02-10 15:21:07 +0000

    fix gitversion to not recompute version from sdist

--- tools/gitversion.py
+++ tools/gitversion.py
@@ -4,17 +4,21 @@ import os
 from pathlib import Path
 import shutil
 
-import versioningit
-
-
 def main():
     project_dir = Path(__file__).resolve().parents[1]
-    vig = versioningit.Versioningit.from_project_dir(project_dir)
-    print(vig.get_version(write=True, fallback=True))
+    source_version_path = project_dir / "src" / "uarray" / "_version.py"
+    if source_version_path.exists():
+        namespace = {}
+        with open(source_version_path, "r", encoding="utf-8") as f:
+            exec(f.read(), namespace)
+        print(namespace["__version__"])
+    else:
+        import versioningit
+        vig = versioningit.Versioningit.from_project_dir(project_dir)
+        print(vig.get_version(write=True, fallback=True))
 
     if meson_dist_root := os.environ.get("MESON_DIST_ROOT"):
         staged_version_path = Path(meson_dist_root) / "src" / "uarray" / "_version.py"
-        source_version_path = project_dir / "src" / "uarray" / "_version.py"
         shutil.copy(source_version_path, staged_version_path)
 
 if __name__ == "__main__":
